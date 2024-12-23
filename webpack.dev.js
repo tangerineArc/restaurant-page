3813b3198a6d960412dfa9cb87af1709c20cdec9
Swagatam <swagatamheroz@outlook.com>
2024-12-23 08:15:58 +0530

    chore: separate webpack configs

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,21 +1,32 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
-module.exports = {
+// const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
     mode: "development",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true
-    },
     devtool: "eval-source-map",
     devServer: {
         watchFiles: ["./src/template.html"]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({ template: "./src/template.html" })
-    ],
+    }
+});
+
+/* module.exports = {
+    // mode: "development",
+    // entry: "./src/index.js",
+    // output: {
+    //     filename: "main.js",
+    //     path: path.resolve(__dirname, "dist"),
+    //     clean: true
+    // },
+    // devtool: "eval-source-map",
+    // devServer: {
+    //     watchFiles: ["./src/template.html"]
+    // },
+    // plugins: [
+    //     new HtmlWebpackPlugin({ template: "./src/template.html" })
+    // ],
     module: {
         rules: [
             {
@@ -32,4 +43,4 @@ module.exports = {
             }
         ]
     }
-}
+} */
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,35 +1,42 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
-module.exports = {
-    mode: "production",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true
-    },
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/template.html"]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({ template: "./src/template.html" })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.html$/i,
-                loader: "html-loader"
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
-                type: "asset/resource"
-            }
-        ]
-    }
-}
+// const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
+    mode: "production"
+});
+
+// module.exports = {
+//     mode: "production",
+//     entry: "./src/index.js",
+//     output: {
+//         filename: "main.js",
+//         path: path.resolve(__dirname, "dist"),
+//         clean: true
+//     },
+//     devtool: "eval-source-map",
+//     devServer: {
+//         watchFiles: ["./src/template.html"]
+//     },
+//     plugins: [
+//         new HtmlWebpackPlugin({ template: "./src/template.html" })
+//     ],
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.css$/i,
+//                 use: ["style-loader", "css-loader"]
+//             },
+//             {
+//                 test: /\.html$/i,
+//                 loader: "html-loader"
+//             },
+//             {
+//                 test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+//                 type: "asset/resource"
+//             }
+//         ]
+//     }
+// };
